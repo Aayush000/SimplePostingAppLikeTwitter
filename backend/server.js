a38c58b3b5499a897da25f7a97628e124fa91698
Aayush <aayushbhatta000@gmail.com>
2022-11-03 15:13:44 -0500

    Added frontend backend and mongodb's remaining code

--- backend/server.js
+++ backend/server.js
@@ -1,218 +1,118 @@
-
-/** Reference code: https://github.com/bpeddapudi/nodejs-basics-routes/blob/master/server.js 
+/** Reference code: https://github.com/bpeddapudi/nodejs-basics-routes/blob/master/server.js
  * import express */
-const express = require('express');
-const cors = require('cors');
-// middleware
-const app = express();
-app.use(express.json());
-app.use(cors())
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-// `npm install mongoose`
-const mongoose = require("mongoose");
+// middleware
+const app = express(); // initialize express
+app.use(express.json()); // to parse the body of the request
+app.use(cors()); // to allow cross-origin requests
+dotenv.config(); // to use .env file
+
 const options = {
-    keepAlive: true,
-    connectTimeoutMS: 10000,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  keepAlive: true,
+  connectTimeoutMS: 10000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
-// mongodb+srv://<username>:<password>@cluster0.6vk0qgz.mongodb.net/?retryWrites=true&w=majority
-// You guys need to replace with your own server url and correct <username> and <password>
-const dbUrl = `mongodb://localhost:27017`;
+
+const dbUrl = process.env.DB_URI; // get the db url from .env file
+const PORT = process.env.PORT; // get the port from .env file
 
 // Mongo DB connection
 mongoose.connect(dbUrl, options, (err) => {
-    if (err) console.log(err);
+  if (err) console.log(err);
 });
 
 // Validate DB connection
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
-    console.log("Mongo DB Connected successfully");
+  console.log("Mongo DB Connected successfully");
 });
 
-// Schema for Book
-let Schema = mongoose.Schema;
-let bookSchema = new Schema(
-    {
-        id: {
-            type: Number,
-        },
-        title: {
-            type: String,
-        },
-        description: {
-            type: String,
-        },
-        author: {
-            type: String,
-        },
-        img: {
-            type: String,
-        },
-        status: {
-            type: String,
-        }
+// Schema for Post
+let postSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
     },
-    { timestamps: true }
+    info: { type: String },
+  },
+  { timestamps: true }
 );
-let BookModel = mongoose.model("book", bookSchema);
 
+let postModel = mongoose.model("post", postSchema);
 
-// Mock Data : We will stop using this static data and will add and remove data from DB itself
-let myMockDB = [
-    {
-        id: 1,
-        title: 'Rich Dad Poor Dad',
-        author: 'Robert Kiyosaki',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptatibus corporis! Deserunt doloribus unde magnam, iusto officia cum commodi praesentium?',
-        img: 'https://m.media-amazon.com/images/I/51AHZGhzZEL.jpg',
-        status: 'todo'
-    },
-    {
-        id: 2,
-        title: 'Rework',
-        author: 'Jason Fried',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptatibus corporis! Deserunt doloribus unde magnam, iusto officia cum commodi praesentium?',
-        img: 'https://m.media-amazon.com/images/P/0307463745.01._SCLZZZZZZZ_SX500_.jpg',
-        status: 'inprogress'
-    },
-    {
-        id: 2,
-        title: 'When Breath Becomes Air',
-        author: 'Paul Kalanithi',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptatibus corporis! Deserunt doloribus unde magnam, iusto officia cum commodi praesentium?',
-        img: 'https://images-na.ssl-images-amazon.com/images/I/71dxZ1Z10xL.jpg',
-        status: 'inprogress'
-    }
-]
-
-app.get('/', (req, res) => {
-    res.send('Your are lucky!! server is running...');
+app.get("/", (req, res) => {
+  res.send("Your are lucky!! server is running...");
 });
-
-
 
 /** GET API: GETs Books from DB and returns as response */
-app.get('/books', async (req, res) => {
-    try {
-        let posts = await BookModel.find();
-        res.status(200).json({
-            status: 200,
-            data: posts,
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 400,
-            message: err.message,
-        });
-    }
+app.get("/posts", async (req, res) => {
+  try {
+    let posts = await postModel.find();
+    res.status(200).json({
+      status: 200,
+      data: posts,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
 });
-
 
 /** POST API: Gets new book info from React and adds it to DB */
-app.post('/books', async (req, res) => {
-    const inputBook = req.body;
-    console.log(inputBook);
-    const matchingBooks = myMockDB.filter(book => book.id === inputBook.id).length;
-    if (matchingBooks) {
-        res.status(500);
-        console.error(`Book with id:${inputBook.id} already exists`);
-    } else {
-        myMockDB.push(req.body);
-    }
-    try {
-        console.log('input Book:', inputBook);
-        let post = new BookModel(inputBook);
-        post = await post.save();
-        res.status(200).json({
-            status: 200,
-            data: post,
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 400,
-            message: err.message,
-        });
-    }
+app.post("/posts", async (req, res) => {
+  const inputPost = req.body;
+
+  try {
+    let post = new postModel(inputPost);
+    post = await post.save();
+    res.status(200).json({
+      status: 200,
+      data: post,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
 });
 
-
-app.put("/books/:bookId", async (req, res) => {
-    try {
-        console.log('PUT request..:' + req.params.bookId);
-        let newBook = req.body;
-        console.log(JSON.stringify('Req body:', JSON.stringify(newBook)));
-        /** There is BUG, Data is not getting updated in DB for me */
-        let book = await BookModel.findByIdAndUpdate({ _id: req.params.bookId }, req.body, {
-            new: true,
-        }).catch((err) => {
-            console.error('Error-----------------', err);
-        });
-        if (book) {
-            console.log(JSON.stringify(book));
-            res.status(200).json({
-                status: 200,
-                data: book,
-            });
-        }
-        res.status(400).json({
-            status: 400,
-            message: "No Book found",
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 400,
-            message: err.message,
-        });
-    }
-});
-
-
-/** DELETE API: Gets ID of the book to be deleted from React and deletes the book in db. 
+/** DELETE API: Gets ID of the book to be deleted from React and deletes the book in db.
  * Sends 400 if there is no book with given id
  * Sends 500 if there is an error while saving data to DB
  * Sends 200 if deleted successfully
  */
-app.delete("/books/:bookId", async (req, res) => {
-    try {
-        let book = await BookModel.findByIdAndRemove(req.params.bookId);
-        if (book) {
-            res.status(200).json({
-                status: 200,
-                message: "Book deleted successfully",
-            });
-        } else {
-            res.status(400).json({
-                status: 400,
-                message: "No Book found",
-            });
-        }
-    } catch (err) {
-        res.status(400).json({
-            status: 400,
-            message: err.message,
-        });
+app.delete("/posts/:postId", async (req, res) => {
+  try {
+    let post = await postModel.findByIdAndRemove(req.params.postId);
+    if (post) {
+      res.status(200).json({
+        status: 200,
+        message: "Post deleted successfully",
+      });
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: "No Post found",
+      });
     }
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
 });
 
-app.get('/loadSampleBooks', async (req, res) => {
-    try {
-        myMockDB.forEach(async (bookIn) => {
-            let post = new BookModel(bookIn);
-            post = await post.save();
-        });
-        res.status(200).json({
-            status: 200,
-            data: myMockDB,
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 400,
-            message: err.message,
-        });
-    }
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-app.listen(3001);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,70 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [posts, setPosts] = useState([{
-    content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo iste quia cum architecto autem porro modi consequuntur provident, quidem adipisci nobis eius deserunt aspernatur inventore!'
-  }],
-    [{
-      content: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia'
-    }]);
+  // Data of Text Area
+  const [posts, setPosts] = useState("");
+  // Refresh state
+  const [refresh, setRefresh] = useState(false);
+  // Data from the backend
+  const [data, setData] = useState([]);
+
+  // Text area change handler
+  const onChangeHandler = (e) => {
+    const { value } = e.target;
+    setPosts(value);
+  };
+
+  // Button click handler
+  const clickHandler = async () => {
+    // Send a POST request to the backend
+    const response = await fetch("http://localhost:3001/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: posts }),
+    });
+
+    if (response.status === 200) {
+      // If the post was created successfully, refresh the page
+      setRefresh(!refresh);
+    }
+
+    // Clear the text area
+    setPosts("");
+  };
+
+  // Delete post handler
+  const handleDelete = async (e) => {
+    // Get the id of the post
+    const id = e.target.parentElement.parentElement.id;
+
+    // Send a DELETE request to the backend
+    const response = await fetch(`http://localhost:3001/posts/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.status === 200) {
+      // If the post was deleted successfully, refresh the page
+      setRefresh(!refresh);
+    }
+  };
+
+  // Get all posts from the backend
+  const getPosts = async () => {
+    const response = await fetch("http://localhost:3001/posts");
+    const data = await response.json();
+    setData(data);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, [refresh]);
+
   return (
-    <div className='react-app-component text-center'>
+    <div className="react-app-component text-center">
       <div className="container">
         <div className="row justify-content-md-center">
           <div className="col-6">
@@ -17,21 +72,46 @@ function App() {
               <div className="card-body">
                 <div className="mb-3">
                   <label className="form-label">Enter your post</label>
-                  <textarea className="form-control" id="post-content" rows="3"></textarea>
+
+                  <textarea
+                    className="form-control"
+                    id="post-content"
+                    rows="3"
+                    onChange={onChangeHandler}
+                    value={posts}
+                  ></textarea>
+
                   <div className="d-grid gap-2">
-                    <button type="button" className="btn btn-primary mt-2">Post</button>
+                    <button
+                      type="button"
+                      className="btn btn-primary mt-2"
+                      onClick={clickHandler}
+                    >
+                      Post
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="card text-white bg-dark my-3 text-start">
-              <div className="card-body">
-                <h6 className="card-subtitle mb-2 text-muted">Oct 4, 2022 - 6:15 PM</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="card-link">Delete</a>
+            {data.map((post) => (
+              <div
+                className="card text-white bg-dark my-3 text-start"
+                id={post._id}
+              >
+                <div className="card-body">
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    {new Date(post.createdAt.split("R")[0]).toLocaleString(
+                      "en-US"
+                    )}
+                  </h6>
+                  <p className="card-text">{post.content}</p>
+                  <a href="#" className="card-link" onClick={handleDelete}>
+                    Delete
+                  </a>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
